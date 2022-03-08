51d1bc16577d0ae9312da18a396920c038d7531f
giacomo13 <giacomomaggio13@gmail.com>
2022-03-08 11:24:10 +0100

    timeout + intervall

--- week02/timeout.js
+++ week02/timeout.js
@@ -5,3 +5,9 @@ const ciao = () => {
 }
 
 setTimeout(ciao,2000); //2 secondi
+
+const id = setInterval (() => {console.log("Ciao in loop")}, 2000) //Interval: ogni 2 secondi riesegui il comando
+
+//clearInterval(id); interrompe l'interval
+
+setTimeout(()=>{clearInterval(id)},5000); //dopo 5 secondi smette di farlo
