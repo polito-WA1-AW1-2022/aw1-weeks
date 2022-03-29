9d2252efff98efd985a180f397a8b906d994f3d8
Enrico Masala <enrico.masala@polito.it>
2022-03-29 11:17:14 +0200

    Esempio click riga

--- week05/exams_script.js
+++ week05/exams_script.js
@@ -6,7 +6,25 @@ p.innerText = d;
 
 document.getElementById('ora').prepend(p);
 
-setInterval( ()=> {p.innerText = dayjs().format('YYYY-MM-DD HH:mm:ss');} , 1000);
+setInterval(() => { p.innerText = dayjs().format('YYYY-MM-DD HH:mm:ss'); }, 1000);
 
 //.innerText = "qui ci va l'ora";
+
+window.addEventListener('load', event => {
+
+    const rows = document.querySelectorAll('table tr');
+
+    for (const row of rows) {
+        row.addEventListener('click', event => {
+            console.log(event.target, row);
+            const voto = row.children[1].innerText;
+
+            const p = document.createElement('p');
+            p.innerText = voto;
+            document.getElementById('comment').appendChild(p);
+        })
+    }
+
+
+})
 
