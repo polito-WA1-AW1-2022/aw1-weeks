4eebab86159ea0bdf835d172e4e171c8984e33eb
Enrico Masala <enrico.masala@polito.it>
2022-03-29 11:31:52 +0200

    Finale

---
+++ week05/exams_script_delete.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let p = document.createElement('p');
+let d = dayjs().format('YYYY-MM-DD HH:mm:ss');
+p.innerText = d;
+
+document.getElementById('ora').prepend(p);
+
+setInterval(() => { p.innerText = dayjs().format('YYYY-MM-DD HH:mm:ss'); }, 1000);
+
+//.innerText = "qui ci va l'ora";
+
+window.addEventListener('load', event => {
+
+    const rows = document.querySelectorAll('table tr');
+
+    for (const row of rows) {
+        /*
+        row.addEventListener('click', event => {
+            console.log(event.target, row);
+            const voto = row.children[1].innerText;
+
+            const p = document.createElement('p');
+            p.innerText = voto;
+            document.getElementById('comment').appendChild(p);
+        })
+        */
+
+        if (row.firstElementChild.tagName.toLowerCase() !== 'th') {
+            row.querySelector('.btn').addEventListener('click', event => {
+                while (row.firstChild)
+                    row.removeChild(row.firstChild)
+                const parent = row.parentNode;
+                parent.removeChild(row);
+            })
+        }
+    }
+
+
+})
+
